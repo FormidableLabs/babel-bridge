9c1629259facba0cea5c07f50abad43b5fff8398
Bryce Kehoe <bryce@kehoe.co.nz>
2023-09-25 16:17:09 +0100

    :sparkles: Add back button to post page

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, Link } from 'react-router-dom';
 
 import { BlogListItem, PortableText } from '../../components';
 
@@ -6,9 +6,13 @@ export default function PostPage() {
   const data = useLoaderData() as {
     post: BlogListItem;
   };
-  const post = data.post;
+  const { post } = data;
+
   return (
     <div className="max-w-4xl mx-auto p-4">
+      <Link to="/" className="block mb-4">
+        🔙
+      </Link>
       <h1 className="text-4xl font-bold mb-6">{post?.title}</h1>
       <PortableText value={post?.body} />
     </div>
