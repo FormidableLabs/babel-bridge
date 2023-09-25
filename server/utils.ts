8769df04c9db8f5aaea889c7db7ccd9ace919fc0
Bryce Kehoe <bryce@kehoe.co.nz>
2023-09-25 12:15:43 +0100

    Server: Update API routing (#15)

--- server/utils.ts
+++ server/utils.ts
@@ -1,7 +1,6 @@
-import { Request } from 'express';
-
-export const getLocale = (req: Request) =>
-  req.headers['accept-language']?.split(',')[0].split(';')[0] ?? 'en-US';
+/** Gets the locale from the Accept-Language header or defaults to 'en-US' .*/
+export const getLocale = (headers: Record<string, string | null>) =>
+  headers['accept-language']?.split(',')[0].split(';')[0] ?? 'en-US';
 
 /** Naively extracts JSON from a plain text string. It assumes that the JSON
  * represents an object, and that the object is the only JSON in the string.
