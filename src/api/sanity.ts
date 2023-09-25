162b6a7bcd81bbe4c80487b264eb3e3f90dd6a5c
Umar <umarg1997@gmail.com>
2023-09-25 12:20:07 +0100

    update api base path

--- src/api/sanity.ts
+++ src/api/sanity.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'http://localhost:3000/api';
 
 export const getDocuments = async () =>
   fetch(`${API_BASE_URL}/posts`)
