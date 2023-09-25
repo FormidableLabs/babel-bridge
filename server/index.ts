6a4081759d0c9472d67c66a9e6b1984128611c9a
Bryce Kehoe <bryce@kehoe.co.nz>
2023-09-25 11:00:15 +0100

    :lock: Enabled CORS for `localhost`

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
 
 import router from './router';
 
 const PORT = 3000;
 
 const app = new Elysia()
+  .use(cors({ origin: /^http:\/\/localhost:\d+$/ }))
   .onError(({ error }) => new Response(error.message, { status: 500 }))
   .group('/api', app => app.use(router))
   .listen(PORT);
