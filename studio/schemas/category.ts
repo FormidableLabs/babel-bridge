ef887fdd0b3241e39388eb952d2f4379f0ef61ea
Bryce Kehoe <bryce@kehoe.co.nz>
2023-09-29 11:16:11 +0100

    Sanity: Add icons to document types (#31)

--- studio/schemas/category.ts
+++ studio/schemas/category.ts
@@ -1,9 +1,11 @@
 import {defineField, defineType} from 'sanity'
+import {TagIcon} from '@sanity/icons'
 
 export default defineType({
   name: 'category',
   title: 'Category',
   type: 'document',
+  icon: TagIcon,
   fields: [
     defineField({
       name: 'title',
