c4f5c76a0fb60675153bb50ccf5cd22b84ff6b64
Umar <umarg1997@gmail.com>
2023-11-15 09:59:54 +0000

    update ui

--- web/components/Post.tsx
+++ web/components/Post.tsx
@@ -10,10 +10,22 @@ export type Post = {
   author: string;
   body: TypedObject;
   _createdAt: string;
-};
+} | null;
 
 export const Post = (props: { post: Post }) => {
   const { post } = props;
+  if (!post) {
+    return (
+      <div className="space-y-6">
+        <div
+          className="yellow-red-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong className="font-bold">Post not found</strong>
+        </div>
+      </div>
+    );
+  }
   return (
     <article>
       <header>
